934c1318f6cdd60741b0ce7ee1038d92e96e2cd0
Najas06 <najasneju@gmail.com>
2025-12-06 01:32:02 +0530

    about us page completed

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -8,10 +8,10 @@ import { cn } from "@/lib/utils";
 import Image from "next/image";
 
 const menuItems = [
-  { name: "Home", href: "#link" },
+  { name: "Home", href: "/" },
   { name: "Products", href: "#link" },
   { name: "Bulk Order", href: "#link" },
-  { name: "About Us", href: "#link" },
+  { name: "About Us", href: "/about-us" },
   { name: "Contact", href: "/contact" },
 ];
 
@@ -65,6 +65,7 @@ export const HeroHeader = () => {
                 <X className="in-data-[state=active]:rotate-0 in-data-[state=active]:scale-100 in-data-[state=active]:opacity-100 absolute inset-0 m-auto size-6 -rotate-180 scale-0 opacity-0 duration-200" />
               </button>
 
+            </div>
               <div className="hidden lg:block">
                 <ul className="flex gap-6 text-sm">
                   {menuItems.map((item, index) => (
@@ -79,7 +80,6 @@ export const HeroHeader = () => {
                   ))}
                 </ul>
               </div>
-            </div>
 
             <div className="bg-background in-data-[state=active]:block lg:in-data-[state=active]:flex mb-6 hidden w-full flex-wrap items-center justify-end space-y-8 rounded-3xl border p-6 shadow-2xl shadow-zinc-300/20 md:flex-nowrap lg:m-0 lg:flex lg:w-fit lg:gap-6 lg:space-y-0 lg:border-transparent lg:bg-transparent lg:p-0 lg:shadow-none dark:shadow-none dark:lg:bg-transparent">
               <div className="lg:hidden">
